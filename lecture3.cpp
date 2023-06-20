d4d3d680e9369e1c3a674e9da44aefcac0c65193
Ramineni Prithvi <95245461+prithvinani@users.noreply.github.com>
2023-06-20 14:27:12 +0530

    Add files via upload

---
+++ lecture3.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    //declare two variabkes anf assign values
+    int a=3,b=5;
+    cout<<"a+b = ";
+    cout<<endl;
+    cout<<a+b;
+    
+    return 0;
+}
